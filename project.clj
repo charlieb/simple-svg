5420834876d0baf06e30c8e77ad81c3e5e053d63
Charlie Burrows <charlie.burrows@gmail.com>
2017-08-19 09:35:45 -0400

    Fixed project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject simple-svg "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject simple-svg "0.1.0"
+  :description "Mini-SVG library to do just what I need and nothing more"
+  :url "https://github.com/charlieb/simple-svg"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
